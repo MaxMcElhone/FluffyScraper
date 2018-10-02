6609a67cdb3fb3be97648eb96146a93749305813
MaxMcElhone <42582959+MaxMcElhone@users.noreply.github.com>
2018-10-02 18:09:38 -0500

    scrape the html and get all of the links

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #Created by Max and Sam
-
+#go_to() for redirecting
+#https://webbot.readthedocs.io/en/latest/webbot.html reference
 from bs4 import BeautifulSoup
 import requests
 from webbot import Browser
 import getpass
+
+root_url='https://stinet.southeasttech.edu/'
 
 print('Enter your sti username:')
 username = input()
@@ -12,7 +15,10 @@ print('Enter your sti password:')
 password = getpass.getpass('password:')
 
 web = Browser()
-web.go_to('https://stinet.southeasttech.edu/ics/')
+web.go_to(root_url)
 web.type(username, id="userName")
 web.type(password, id="password")
 web.click(id="siteNavBar_btnLogin")
+soup = BeautifulSoup(web.get_page_source())
+for link in soup.find_all('a'):
+    print(link.get('href'))
