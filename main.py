4aabc2186c761814b098c876c661312ed502ad65
McElhone <Maxwell.McElhone@southeasttech.edu>
2018-10-02 12:32:42 -0500

    changed the password input type
    I made the password input field not show what is being typed.

--- main.py
+++ main.py
@@ -3,16 +3,16 @@
 from bs4 import BeautifulSoup
 import requests
 from webbot import Browser
+import getpass
 
 print('Enter your sti username:')
 username = input()
 
 print('Enter your sti password:')
-password = input()
+password = getpass.getpass('password:')
 
 web = Browser()
 web.go_to('https://stinet.southeasttech.edu/ics/')
 web.type(username, id="userName")
 web.type(password, id="password")
 web.click(id="siteNavBar_btnLogin")
-
