c502da209c4d33eed70a71ee3e85a3b48d3081c9
troot <sam_d_olson@hotmail.com>
2018-10-02 11:39:58 -0500

    added webbot

--- main.py
+++ main.py
@@ -2,4 +2,17 @@
 
 from bs4 import BeautifulSoup
 import requests
+from webbot import Browser
+
+print('Enter your sti username:')
+username = input()
+
+print('Enter your sti password:')
+password = input()
+
+web = Browser()
+web.go_to('https://stinet.southeasttech.edu/ics/')
+web.type(username, id="userName")
+web.type(password, id="password")
+web.click(id="siteNavBar_btnLogin")
 
