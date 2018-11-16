dd39e2efaf02a4780ad81b6ad08629a132db843c
MaxMcElhone <Maxwell.McElhone@gmail.com>
2018-11-16 16:59:24 -0600

    Fixed the problem of it crashing if you have less than two upcomming assignments

--- home/models.py
+++ home/models.py
@@ -40,23 +40,50 @@ class Coursework:
             soup = BeautifulSoup(web.get_page_source(), "html5lib")
             for data in soup.select('.sidebar-link-title a'):
                 className = data.text
-            assignment1 = soup.find(id='pg0_V__dueNext__rptDueNext_ctl00__hypAssign')
-            assignment2 = soup.find(id='pg0_V__dueNext__rptDueNext_ctl01__hypAssign')
-
+            #assignment1 = soup.find(id='pg0_V__dueNext__rptDueNext_ctl00__hypAssign')
+            #assignment2 = soup.find(id='pg0_V__dueNext__rptDueNext_ctl01__hypAssign')
+            assignment1 = try_find(soup, 'pg0_V__dueNext__rptDueNext_ctl00__hypAssign')
+            assignment2 = try_find(soup,'pg0_V__dueNext__rptDueNext_ctl01__hypAssign')
             course = {
                 'courseName': className,
                 'courseUrl': root_url + page + "Coursework.jnz",
-                'assignment1': assignment1.text,
-                'assignment1Desc': assignment1.get('aria-label'),
-                'assignment1Link': root_url + assignment1.get('href'),
-                'assignment1Progress': soup.find(id='pg0_V__dueNext__rptDueNext_ctl00__lblInfo').text,
-                'assignment2': assignment2.text,
-                'assignment2Desc': assignment2.get('aria-label'),
-                'assignment2Link': root_url + assignment2.get('href'),
-                'assignment2Progress': soup.find(id='pg0_V__dueNext__rptDueNext_ctl01__lblInfo').text,
             }
+
+
+
+            # course = {
+            #     'courseName': className,
+            #     'courseUrl': root_url + page + "Coursework.jnz",
+
+            if assignment1 is not None:
+                course['assignment1'] = assignment1.text
+                course['assignment1Desc'] = assignment1.get('aria-label')
+                course['assignment1Link'] = root_url + assignment1.get('href')
+                course['assignment1Progress'] = soup.find(id='pg0_V__dueNext__rptDueNext_ctl00__lblInfo').text
+            else:
+                course['assignment1'] = ''
+                course['assignment1Desc'] = ''
+                course['assignment1Link'] = ''
+                course['assignment1Progress'] = ''
+            if assignment2 is not None:
+                course['assignment2'] = assignment2.text
+                course['assignment2Desc'] = assignment2.get('aria-label')
+                course['assignment2Link'] = root_url + assignment2.get('href')
+                course['assignment2Progress'] = soup.find(id='pg0_V__dueNext__rptDueNext_ctl01__lblInfo').text
+            else:
+                course['assignment2'] = ''
+                course['assignment2Desc'] = ''
+                course['assignment2Link'] = ''
+                course['assignment2Progress'] = ''
+            # }
 
             coursework.append(course.copy())
 
         web.close_current_tag()
         return coursework;
+
+def try_find(soup, idIn):
+    try:
+        return soup.find(id=idIn)
+    except:
+        return None
